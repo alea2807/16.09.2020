b66088fdc8c8b1081da09d7a232d9d018e97f921
alexandra605 <70931822+alexandra605@users.noreply.github.com>
2020-09-15 14:14:51 +0300

    Add files via upload

---
+++ cl 10 C.py
@@ -0,0 +1,13 @@
+print("Alexandra Dubenco")
+print("eleva al Liceului Teoretic Mihai Eminescu")
+print("15 ani din Moldova")
+
+print("Date de contact")
+print("telefon 076798568")
+print("adresa alexandradubenco06052005@gmail.com")
+
+print("despre mine")
+print("obiectivul de a cunoaste cit mai multe limbi straine")
+
+print("experienta profesioanala")
+print("10 ani de liceu")
